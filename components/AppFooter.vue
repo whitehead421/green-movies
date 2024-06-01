97053ea20f02f06c5df2ff9746112509144e088a
Hüseyin Akbaş <huseyinakbasblog@gmail.com>
2024-06-02 01:18:40 +0300

    fix: remove unnecessary div

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,13 +1,11 @@
 <template>
-  <footer>
-    <div class="flex flex-col gap-4 p-4">
-      <div class="text-center text-gray-400 font-light text-sm">
-        <p>&copy; 2024 Green Movies. All rights reserved.</p>
-        <p>
-          Designed by <span class="text-black font-black">Simple</span
-          ><span class="text-black font-light">Thing</span>
-        </p>
-      </div>
+  <footer class="flex flex-col gap-4 p-4">
+    <div class="text-center text-gray-400 font-light text-sm">
+      <p>&copy; 2024 Green Movies. All rights reserved.</p>
+      <p>
+        Designed by <span class="text-black font-black">Simple</span
+        ><span class="text-black font-light">Thing</span>
+      </p>
     </div>
   </footer>
 </template>
