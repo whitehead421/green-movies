f587a597077e2c81f508d2a6987ee1f77084abee
Hüseyin Akbaş <huseyinakbasblog@gmail.com>
2024-06-02 16:12:14 +0300

    feat: add sticky to parts component

--- components/ui/Parts.vue
+++ components/ui/Parts.vue
@@ -1,5 +1,5 @@
 <template>
-  <aside class="flex flex-col gap-4">
+  <aside class="flex flex-col gap-4 h-min sticky top-4">
     <ul v-for="(segment, index) in data" :key="index">
       <li class="text-sm font-semibold leading-none">{{ segment.label }}</li>
       <li class="text-gray-400">{{ segment.subLabel }}</li>
